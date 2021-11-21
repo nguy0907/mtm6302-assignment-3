05630fe1a7436829e02212aebfcfd77a09a39f5d
nguy0907 <nguy0907@algonquinlive.com>
2021-11-21 03:11:31 -0500

    adding html

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 const $container = document.getElementById('container')
-const date = new Date()
+const $currentTime = document.getElementById('currentTime')
+const $date = document.getElementById('date')
+const $day = document.getElementById('day')
+const $monthSelector = new Date()
+const today = new Date()
+
+// const month = {
+//     january: 'January',
+//     february: 'February',
+//     march: 'March',
+//     april: 'April',
+//     may: 'May',
+//     june: 'June',
+//     july: 'July',
+//     august: 'august',
+//     september: 'September',
+//     october:'October',
+//     november: 'November',
+//     december: 'December'
+// }
 
 const month = [
     'January',
@@ -9,11 +28,17 @@ const month = [
     'May',
     'June',
     'July',
-    'August',
+    'august',
     'September',
     'October',
     'November',
     'December'
 ]
 
-const daysInMonth = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
+$currentTime.innerText = today
+
+console.log($monthSelector.getMonth())
+
+
+const daysInMonth = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
+

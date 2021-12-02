186587174840bc66e601e001b413b60fd3e7f474
nguy0907 <nguy0907@algonquinlive.com>
2021-12-02 12:16:32 -0500

    adding js

--- script.js
+++ script.js
@@ -30,102 +30,138 @@ const month = [
     'December'
 ]
 
-//Local Storage
-localStorage.setItem('month',$monthSelector.value)
+const test = document.getElementById('test')
 
 //function to get the timer counting down
+for (let days = 0; days <31; days++){   
+    $daySelector.innerHTML += `<option value="${daysInMonth[days]}">${daysInMonth[days]}</option>`
+    }
+
+function upto31days(){
+for (let days = 0; days <31; days++){   
+$daySelector.innerHTML += `<option value="${daysInMonth[days]}">${daysInMonth[days]}</option>`
+}
+}
+function upto28days(){
+    for (let days = 0; days <28; days++){
+        $daySelector.innerHTML += `<option value="${daysInMonth[days]}">${daysInMonth[days]}</option>`
+        }
+        }
 
 
-//Function to update the time
-// function updateTime(){
-//     $day.textContent = today.getDay()
-//     $year.textContent = today.getFullYear()
-// }
+function upto29days(){
+    for (let days = 0; days <29; days++){
+        $daySelector.innerHTML += `<option value="${daysInMonth[days]}">${daysInMonth[days]}</option>`
+        }
+        }
 
-//Function for month and year selection
-
-
-
-
-// $currentTime.innerText = today
-
-
+function upto30days(){
+for (let days = 0; days <30; days++){
+    
+    $daySelector.innerHTML += `<option value="${daysInMonth[days]}">${daysInMonth[days]}</option>`
+    }
+    }
 
 $monthSelector.addEventListener('change', function () {
     switch ($monthSelector.value) {
        //January
-        case '0':
-        
+        case 'January':
+          
             // futureTime.setMonth(0)
-            futureTime.setFullYear(2022,0,1)
+            futureTime.setFullYear(2022,0)
+            $daySelector.innerHTML = ` `
+            upto31days()
             break
 //February
-        case '1':
-            upto28days()
+        case 'February':
+           
             // futureTime.setMonth(1)
-            futureTime.setFullYear(2022,1,1)
+            futureTime.setFullYear(2022,1)
+            $daySelector.innerHTML = ` ` 
+            upto28days()
             break
 //March
-        case '2':
+        case 'March':
      
             // futureTime.setMonth(2)
-            futureTime.setFullYear(2022,2,1)
+            futureTime.setFullYear(2022,2)
+            $daySelector.innerHTML = ` ` 
+            upto31days()
             break
 //April
-        case '3':
-            upto30days()
+        case 'April':
+            
             // futureTime.setMonth(3)
-            futureTime.setFullYear(2022,3,1)
+            futureTime.setFullYear(2022,3)
+            $daySelector.innerHTML = ` ` 
+            upto30days()
             break
 //May
-        case '4':
+        case 'May':
        
             // futureTime.setMonth(4)
-            futureTime.setFullYear(2022,4,1)
+            futureTime.setFullYear(2022,4)
+            $daySelector.innerHTML = ` `
+            upto31days() 
             break
 //June
-        case '5':
-            upto30days()
+        case 'June':
+           
             // futureTime.setMonth(5)
-            futureTime.setFullYear(2022,5,1)
+            futureTime.setFullYear(2022,5)
+            $daySelector.innerHTML = ` ` 
+            upto30days()
             break
 //July
-        case '6':
+        case 'July':
     
             // futureTime.setMonth(6)
-            futureTime.setFullYear(2022,6,1)
+            futureTime.setFullYear(2022,6)
+            $daySelector.innerHTML = ` ` 
+            upto31days() 
             break
 //August
-        case '7':
+        case 'August':
        
             // futureTime.setMonth(7)
-            futureTime.setFullYear(2022,7,1)
+            futureTime.setFullYear(2022,7)
+            $daySelector.innerHTML = ` ` 
+            upto31days() 
             break
 //September
-        case '8':
-            upto30days()
+        case 'September':
+            
             // futureTime.setMonth(8)
-            futureTime.setFullYear(2022,8,1)
+            futureTime.setFullYear(2022,8)
+            $daySelector.innerHTML = ` ` 
+            upto30days()
             break
 //October
-        case '9':
+        case 'October':
    
             // futureTime.setMonth(9)
-            futureTime.setFullYear(2022,9,1)
+            futureTime.setFullYear(2022,9)
+            $daySelector.innerHTML = ` ` 
+            upto31days()
             console.log(futureTime.getDate())
 
             break
 //November
-        case '10':
-            upto29days()
-            futureTime.setFullYear(2022,10,1)
+        case 'November':
+         
+            futureTime.setFullYear(2022,10)
+            $daySelector.innerHTML = ` ` 
+               upto30days()
             // futureTime.setMonth(10)
               console.log(futureTime.getDate())    
             break
 //December
-        case '11':
-            upto30days()
+        case 'December':
+            
+           
             futureTime.setFullYear(2021,11,1)
+            $daySelector.innerHTML = ` ` 
+             upto30days()
             // futureTime.setMonth(11)
             console.log(futureTime.getDate())
             break
@@ -160,127 +196,80 @@ $daySelector.addEventListener('change', function(){
     }
 })
 
-function upto28days (){
+
+
+// function upto29days (){
     
-    $daySelector.innerHTML = `<select id="daySelector">
-   <option value="0">${daysInMonth[0]}</option>
-   <option value="1">${daysInMonth[1]}</option>
-   <option value="2">${daysInMonth[2]}</option>
-   <option value="3">${daysInMonth[3]}</option>
-   <option value="4">${daysInMonth[4]}</option>
-   <option value="5">${daysInMonth[5]}</option>
-   <option value="6">${daysInMonth[6]}</option>
-   <option value="7">${daysInMonth[7]}</option>
-   <option value="8">${daysInMonth[8]}</option>
-   <option value="9">${daysInMonth[9]}</option>
-   <option value="10">${daysInMonth[10]}</option>
-   <option value="11">${daysInMonth[11]}</option>
-   <option value="12">${daysInMonth[12]}</option>
-   <option value="13">${daysInMonth[13]}</option>
-   <option value="14">${daysInMonth[14]}</option>
-   <option value="15">${daysInMonth[15]}</option>
-   <option value="16">${daysInMonth[16]}</option>
-   <option value="17">${daysInMonth[17]}</option>
-   <option value="18">${daysInMonth[18]}</option>
-   <option value="19">${daysInMonth[19]}</option>
-   <option value="20">${daysInMonth[20]}</option>
-   <option value="21">${daysInMonth[21]}</option>
-   <option value="22">${daysInMonth[22]}</option>
-   <option value="23">${daysInMonth[23]}</option>
-   <option value="24">${daysInMonth[24]}</option>
-   <option value="25">${daysInMonth[25]}</option>
-   <option value="26">${daysInMonth[26]}</option>
-   <option value="27">${daysInMonth[27]}</option>
-   <option value="28">${daysInMonth[28]}</option>
-   </select>` 
-   }
-   
+//  $daySelector.innerHTML = `<select id="daySelector">
+// <option value="0">${daysInMonth[0]}</option>
+// <option value="1">${daysInMonth[1]}</option>
+// <option value="2">${daysInMonth[2]}</option>
+// <option value="3">${daysInMonth[3]}</option>
+// <option value="4">${daysInMonth[4]}</option>
+// <option value="5">${daysInMonth[5]}</option>
+// <option value="6">${daysInMonth[6]}</option>
+// <option value="7">${daysInMonth[7]}</option>
+// <option value="8">${daysInMonth[8]}</option>
+// <option value="9">${daysInMonth[9]}</option>
+// <option value="10">${daysInMonth[10]}</option>
+// <option value="11">${daysInMonth[11]}</option>
+// <option value="12">${daysInMonth[12]}</option>
+// <option value="13">${daysInMonth[13]}</option>
+// <option value="14">${daysInMonth[14]}</option>
+// <option value="15">${daysInMonth[15]}</option>
+// <option value="16">${daysInMonth[16]}</option>
+// <option value="17">${daysInMonth[17]}</option>
+// <option value="18">${daysInMonth[18]}</option>
+// <option value="19">${daysInMonth[19]}</option>
+// <option value="20">${daysInMonth[20]}</option>
+// <option value="21">${daysInMonth[21]}</option>
+// <option value="22">${daysInMonth[22]}</option>
+// <option value="23">${daysInMonth[23]}</option>
+// <option value="24">${daysInMonth[24]}</option>
+// <option value="25">${daysInMonth[25]}</option>
+// <option value="26">${daysInMonth[26]}</option>
+// <option value="27">${daysInMonth[27]}</option>
+// <option value="28">${daysInMonth[28]}</option>
+// <option value="29">${daysInMonth[29]}</option>
 
-function upto29days (){
-    
- $daySelector.innerHTML = `<select id="daySelector">
-<option value="0">${daysInMonth[0]}</option>
-<option value="1">${daysInMonth[1]}</option>
-<option value="2">${daysInMonth[2]}</option>
-<option value="3">${daysInMonth[3]}</option>
-<option value="4">${daysInMonth[4]}</option>
-<option value="5">${daysInMonth[5]}</option>
-<option value="6">${daysInMonth[6]}</option>
-<option value="7">${daysInMonth[7]}</option>
-<option value="8">${daysInMonth[8]}</option>
-<option value="9">${daysInMonth[9]}</option>
-<option value="10">${daysInMonth[10]}</option>
-<option value="11">${daysInMonth[11]}</option>
-<option value="12">${daysInMonth[12]}</option>
-<option value="13">${daysInMonth[13]}</option>
-<option value="14">${daysInMonth[14]}</option>
-<option value="15">${daysInMonth[15]}</option>
-<option value="16">${daysInMonth[16]}</option>
-<option value="17">${daysInMonth[17]}</option>
-<option value="18">${daysInMonth[18]}</option>
-<option value="19">${daysInMonth[19]}</option>
-<option value="20">${daysInMonth[20]}</option>
-<option value="21">${daysInMonth[21]}</option>
-<option value="22">${daysInMonth[22]}</option>
-<option value="23">${daysInMonth[23]}</option>
-<option value="24">${daysInMonth[24]}</option>
-<option value="25">${daysInMonth[25]}</option>
-<option value="26">${daysInMonth[26]}</option>
-<option value="27">${daysInMonth[27]}</option>
-<option value="28">${daysInMonth[28]}</option>
-<option value="29">${daysInMonth[29]}</option>
+// </select>` 
+// }
 
-</select>` 
-}
 
-function upto30days(){
-$daySelector.innerHTML = `<select id="daySelector">
-<option value="0">${daysInMonth[0]}</option>
-<option value="1">${daysInMonth[1]}</option>
-<option value="2">${daysInMonth[2]}</option>
-<option value="3">${daysInMonth[3]}</option>
-<option value="4">${daysInMonth[4]}</option>
-<option value="5">${daysInMonth[5]}</option>
-<option value="6">${daysInMonth[6]}</option>
-<option value="7">${daysInMonth[7]}</option>
-<option value="8">${daysInMonth[8]}</option>
-<option value="9">${daysInMonth[9]}</option>
-<option value="10">${daysInMonth[10]}</option>
-<option value="11">${daysInMonth[11]}</option>
-<option value="12">${daysInMonth[12]}</option>
-<option value="13">${daysInMonth[13]}</option>
-<option value="14">${daysInMonth[14]}</option>
-<option value="15">${daysInMonth[15]}</option>
-<option value="16">${daysInMonth[16]}</option>
-<option value="17">${daysInMonth[17]}</option>
-<option value="18">${daysInMonth[18]}</option>
-<option value="19">${daysInMonth[19]}</option>
-<option value="20">${daysInMonth[20]}</option>
-<option value="21">${daysInMonth[21]}</option>
-<option value="22">${daysInMonth[22]}</option>
-<option value="23">${daysInMonth[23]}</option>
-<option value="24">${daysInMonth[24]}</option>
-<option value="25">${daysInMonth[25]}</option>
-<option value="26">${daysInMonth[26]}</option>
-<option value="27">${daysInMonth[27]}</option>
-<option value="28">${daysInMonth[28]}</option>
-<option value="29">${daysInMonth[29]}</option>
-<option value="30">${daysInMonth[30]}</option>
+// function getLocalTime(time){
+//     let UTCTime = new Date()
+//     const offsets = month[time]
 
-</select>` 
-}
+//     let hours = parseInt(UTCTime.slice(0, 2)) + offset
+
+//     if (hours >= 24) {
+//         hours -= 24
+//     } else if (hours < 0) {
+//         hours += 24
+//     }
+// }
 
 
 
+// function showdata(){
+//     $selectedDate.innerText = futureTime + ` 
+//     <p id='time-display'>${getLocalTime(city)}</p>
+//     <button id='update'>Update</button>`
 
+//     document,getElementById('update').addEventListener('click', function (){
 
-if ($monthSelector === $monthSelector.value && $daySelector === $daySelector.value){
-    console.log('hi')
-}
+//     })
+
+// }
+
 
 $showdate.addEventListener('click', function(){
     $selectedDate.innerText = futureTime
-    console.log(`Date: ${localStorage.getItem('month')}`)
+   
+//Local Storage
+localStorage.setItem('date',futureTime)
+
+let differentdate = 0
 
 let difference = futureTime.getTime() - today.getTime()
 
@@ -313,7 +302,7 @@ function toSeconds(ms){
     const minutes = toMinutes(ms)
     const seconds = Math.floor(ms / 1000)
 
-    const secondsleft = seconds - minutes - (hours * 60) - (days * 24 * 60)
+    const secondsleft = seconds - (minutes) - (hours * 60) - (days * 24)
 
 return secondsleft 
 }
@@ -335,7 +324,7 @@ TimerCountdown = setInterval(function(){
 
 })
 
-//countdown
+
 
 
   
